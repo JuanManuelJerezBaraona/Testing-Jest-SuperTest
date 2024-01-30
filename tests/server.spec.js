36c56c7ce7d12af3cbbd8fad1dab8f6e72bd1ba0
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-30 18:33:37 -0300

    Add faker-js library for generating fake IDs in delete test

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,6 +1,7 @@
 const request = require("supertest");
 const server = require("../index");
 
+const { faker } = require('@faker-js/faker');
 const generateJWT = require("./generate.jwt");
 
 describe("Operaciones CRUD de cafes", () => {
@@ -13,9 +14,13 @@ describe("Operaciones CRUD de cafes", () => {
     });
 
     it("DELETE /cafes/:id Comprueba que se obtiene un código 404 al intentar eliminar un café con un id que no existe.", async () => {
+        let fakeID = faker.string.numeric([1, 2, 3, 4]); // Genera un id aleatorio excluyendo el 1, 2, 3 y 4
         const token = generateJWT();
-        const response = await request(server).delete("/cafes/99").set("Authorization", `Bearer ${token}`).send();
+
+        const response = await request(server).delete(`/cafes/${fakeID}`).set("Authorization", `Bearer ${token}`).send();
         expect(response.status).toBe(404);
     });
+
+
 
 });
