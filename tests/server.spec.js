108a9a175bf16dfc4af30359dca5f4af144b769b
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-30 19:26:32 -0300

    Refactor server import and add new test for POST /cafes

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const server = require("../index");
+const app = require("../index");
 
 const { faker } = require('@faker-js/faker');
 const generateJWT = require("./generate.jwt");
@@ -7,20 +7,28 @@ const generateJWT = require("./generate.jwt");
 describe("Operaciones CRUD de cafes", () => {
 
     it("GET /cafes Teste que se devuelve un status code 200 y el tipo de dato recibido es un arreglo con por lo menos 1 objeto", async () => {
-        const response = await request(server).get("/cafes").send();
+        const response = await request(app).get("/cafes").send();
         expect(response.status).toBe(200);
         expect(response.body).toBeInstanceOf(Array);
         expect(response.body.length).toBeGreaterThan(0);
     });
 
     it("DELETE /cafes/:id Comprueba que se obtiene un código 404 al intentar eliminar un café con un id que no existe.", async () => {
-        let fakeID = faker.string.numeric([1, 2, 3, 4]); // Genera un id aleatorio excluyendo el 1, 2, 3 y 4
+        let fakeID = faker.number.int({ min: 5, max: 100 })
         const token = generateJWT();
 
-        const response = await request(server).delete(`/cafes/${fakeID}`).set("Authorization", `Bearer ${token}`).send();
+        const response = await request(app).delete(`/cafes/${fakeID}`).set("Authorization", `Bearer ${token}`).send();
         expect(response.status).toBe(404);
     });
 
+    it("Prueba que la ruta POST /cafes agrega un nuevo café y devuelve un código 201", async () => {
+        const payload = {
+            id: faker.number.int({ min: 5, max: 100 }),
+            nombre: faker.commerce.productName()
+        };
 
+        const response = await request(app).post("/cafes").send(payload);
+        expect(response.status).toBe(201);
+    });
 
 });
