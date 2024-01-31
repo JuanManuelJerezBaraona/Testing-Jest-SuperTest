3d7564a2f39ac652ec80ac209cf3dc56bd3b3a72
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-01-31 08:50:53 -0300

    Refactor tests and update package description

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -4,24 +4,24 @@ const app = require("../index");
 const { faker } = require('@faker-js/faker');
 const generateJWT = require("./generate.jwt");
 
-describe("Operaciones CRUD de cafes", () => {
+describe("CRUD operations for coffees", () => {
 
-    it("GET /cafes Teste que se devuelve un status code 200 y el tipo de dato recibido es un arreglo con por lo menos 1 objeto", async () => {
+    it("GET /cafes should return status code 200 and an array with at least 1 object", async () => {
         const response = await request(app).get("/cafes").send();
         expect(response.status).toBe(200);
         expect(response.body).toBeInstanceOf(Array);
         expect(response.body.length).toBeGreaterThan(0);
     });
 
-    it("DELETE /cafes/:id Comprueba que se obtiene un código 404 al intentar eliminar un café con un id que no existe.", async () => {
-        let fakeID = faker.number.int({ min: 5, max: 100 })
+    it("DELETE /cafes/:id should return status code 404 when trying to delete a coffee with an id that does not exist", async () => {
+        let nonExistingId = faker.number.int({ min: 5, max: 100 })
         const token = generateJWT();
 
-        const response = await request(app).delete(`/cafes/${fakeID}`).set("Authorization", `Bearer ${token}`).send();
+        const response = await request(app).delete(`/cafes/${nonExistingId}`).set("Authorization", `Bearer ${token}`).send();
         expect(response.status).toBe(404);
     });
 
-    it("Prueba que la ruta POST /cafes agrega un nuevo café y devuelve un código 201", async () => {
+    it("POST /cafes should add a new coffee and return status code 201", async () => {
         const payload = {
             id: faker.number.int({ min: 5, max: 100 }),
             nombre: faker.commerce.productName()
@@ -31,15 +31,15 @@ describe("Operaciones CRUD de cafes", () => {
         expect(response.status).toBe(201);
     });
 
-    it("Prueba que la ruta PUT /cafes devuelve un status code 400 si intentas actualizar un café enviando un id en los parámetros que sea diferente al id dentro del payload", async () => {
+    it("PUT /cafes should return status code 400 if id in parameters is different from id in payload", async () => {
         const payload = {
             id: faker.number.int({ min: 5, max: 100 }),
             nombre: faker.commerce.productName()
         };
 
-        let fakeID = faker.number.int({ min: 101, max: 200 })
+        let idInParameters = faker.number.int({ min: 101, max: 200 })
 
-        const response = await request(app).put(`/cafes/${fakeID}`).send(payload);
+        const response = await request(app).put(`/cafes/${idInParameters}`).send(payload);
         expect(response.status).toBe(400);
     });
 });
