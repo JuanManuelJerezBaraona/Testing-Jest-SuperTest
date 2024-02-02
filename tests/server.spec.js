9c24f53858279b5eda21f13e3babd2da2e974785
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2024-02-02 08:46:06 -0300

    Remove generate.jwt.js and update import in server.spec.js

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../index");
 
 const { faker } = require('@faker-js/faker');
-const generateJWT = require("./generate.jwt");
+const generateJWT = require('../helpers/generate.jwt');
 
 describe("Read operations for /cafes", () => {
 
